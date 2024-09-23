ee929a791b125f29aebf571bf94e2e3213af69a2
Henry <genomikel@gmail.com>
2024-09-22 22:25:31 -0500

    updating port to run

--- src/LogisticaApi.Api/Program.cs
+++ src/LogisticaApi.Api/Program.cs
@@ -12,6 +12,7 @@ namespace RetailProductMicroservice.Api
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://*:8085");
                 });
     }
 }
